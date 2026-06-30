50885ac6374416b31af4471739186b7a6803a3bc
Brandon <77804246+Bogrodnik@users.noreply.github.com>
2026-06-30 17:39:15 -0400

    Add GitHub Pages deployment workflow

--- src/data/eternals.js
+++ src/data/eternals.js
@@ -4,7 +4,7 @@ export const eternals = [
 id:"dahla",
 name:"Dahla",
 setBonus:"Deep Pockets",
-image:"/images/eternals/dahla.png",
+image:"https://arkheron.wiki.gg/images/thumb/Arkheron_Eternal_Dahla.png/292px-Arkheron_Eternal_Dahla.png?2f2829",
 wiki:"https://arkheron.wiki.gg/wiki/Dahla"
 },
 
@@ -12,7 +12,7 @@ wiki:"https://arkheron.wiki.gg/wiki/Dahla"
 id:"edani",
 name:"Edani",
 setBonus:"Ferocious",
-image:"/images/eternals/edani.png",
+image:"https://arkheron.wiki.gg/images/thumb/Arkheron_Eternal_Edani.png/242px-Arkheron_Eternal_Edani.png?21274a",
 wiki:"https://arkheron.wiki.gg/wiki/Edani"
 },
 
@@ -20,7 +20,7 @@ wiki:"https://arkheron.wiki.gg/wiki/Edani"
 id:"irenna",
 name:"Irenna",
 setBonus:"Cold-Blooded",
-image:"/images/eternals/irenna.png",
+image:"https://arkheron.wiki.gg/images/thumb/Arkheron_Eternal_Irenna.png/269px-Arkheron_Eternal_Irenna.png?da4802",
 wiki:"https://arkheron.wiki.gg/wiki/Irenna"
 },
 
@@ -28,7 +28,7 @@ wiki:"https://arkheron.wiki.gg/wiki/Irenna"
 id:"ravah",
 name:"Ravah",
 setBonus:"Distant",
-image:"/images/eternals/ravah.png",
+image:"https://arkheron.wiki.gg/images/thumb/Arkheron_Eternal_Ravah.png/201px-Arkheron_Eternal_Ravah.png?903b61",
 wiki:"https://arkheron.wiki.gg/wiki/Ravah"
 },
 
@@ -36,7 +36,7 @@ wiki:"https://arkheron.wiki.gg/wiki/Ravah"
 id:"hollow",
 name:"Hollow",
 setBonus:"Unscathed",
-image:"/images/eternals/hollow.png",
+image:"https://arkheron.wiki.gg/images/thumb/Arkheron_Eternal_Hollow.png/185px-Arkheron_Eternal_Hollow.png?7cfe4b",
 wiki:"https://arkheron.wiki.gg/wiki/Hollow"
 },
 
@@ -44,7 +44,7 @@ wiki:"https://arkheron.wiki.gg/wiki/Hollow"
 id:"leodin",
 name:"Leodin",
 setBonus:"Divine",
-image:"/images/eternals/leodin.png",
+image:"https://arkheron.wiki.gg/images/thumb/Arkheron_Eternal_Leodin.png/314px-Arkheron_Eternal_Leodin.png?8c9183",
 wiki:"https://arkheron.wiki.gg/wiki/Leodin"
 },
 
@@ -52,7 +52,7 @@ wiki:"https://arkheron.wiki.gg/wiki/Leodin"
 id:"karriv",
 name:"Karriv",
 setBonus:"Persistent",
-image:"/images/eternals/karriv.png",
+image:"https://arkheron.wiki.gg/images/thumb/Arkheron_Eternal_Karriv.png/214px-Arkheron_Eternal_Karriv.png?f5460e",
 wiki:"https://arkheron.wiki.gg/wiki/Karriv"
 },
 
@@ -60,7 +60,7 @@ wiki:"https://arkheron.wiki.gg/wiki/Karriv"
 id:"rynshi",
 name:"Rynshi",
 setBonus:"Relentless",
-image:"/images/eternals/rynshi.png",
+image:"https://arkheron.wiki.gg/images/thumb/Arkheron_Eternal_Rynshi.png/216px-Arkheron_Eternal_Rynshi.png?ab2d9c",
 wiki:"https://arkheron.wiki.gg/wiki/Rynshi"
 },
 
@@ -68,7 +68,7 @@ wiki:"https://arkheron.wiki.gg/wiki/Rynshi"
 id:"vaton",
 name:"Vaton",
 setBonus:"Malleable",
-image:"/images/eternals/vaton.png",
+image:"https://arkheron.wiki.gg/images/thumb/Arkheron_Eternal_Vaton.png/231px-Arkheron_Eternal_Vaton.png?93f3d8",
 wiki:"https://arkheron.wiki.gg/wiki/Vaton"
 },
 
@@ -76,7 +76,7 @@ wiki:"https://arkheron.wiki.gg/wiki/Vaton"
 id:"grimwold",
 name:"Grimwold",
 setBonus:"Galvanized",
-image:"/images/eternals/grimwold.png",
+image:"https://arkheron.wiki.gg/images/thumb/Arkheron_Eternal_Grimwold.png/178px-Arkheron_Eternal_Grimwold.png?56a41e",
 wiki:"https://arkheron.wiki.gg/wiki/Grimwold"
 },
 
@@ -84,7 +84,7 @@ wiki:"https://arkheron.wiki.gg/wiki/Grimwold"
 id:"penelope",
 name:"Penelope",
 setBonus:"Youthful",
-image:"/images/eternals/penelope.png",
+image:"https://arkheron.wiki.gg/images/thumb/Arkheron_Eternal_Penelope.png/301px-Arkheron_Eternal_Penelope.png?b30911",
 wiki:"https://arkheron.wiki.gg/wiki/Penelope"
 },
 
@@ -92,7 +92,7 @@ wiki:"https://arkheron.wiki.gg/wiki/Penelope"
 id:"tsubo",
 name:"Tsu'bo",
 setBonus:"Endling",
-image:"/images/eternals/tsubo.png",
+image:"https://arkheron.wiki.gg/images/thumb/Arkheron_Eternal_Tsu%27bo.png/342px-Arkheron_Eternal_Tsu%27bo.png?5347eb",
 wiki:"https://arkheron.wiki.gg/wiki/Tsu'bo"
 }
 
